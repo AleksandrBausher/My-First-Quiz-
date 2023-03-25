128dcaa1e38c0a9be4359758a7f5106e2447acf8
AleksandrBausher <bausheraleksandr@gmail.com>
2023-03-25 16:50:45 -0400

    created questions array

--- script.js
+++ script.js
@@ -4,24 +4,107 @@ let questionsScreen = document.getElementById("questionsScreen");
 let quizFinishedScreen = document.getElementById("quizFinishedScreen");
 let highScoresScreen = document.getElementById("highScoresScreen");
 let topBar = document.getElementById("topBar");
+let optionWrapper = document.getElementById("optionWrapper");
 
 //Buttons
 let startQuizButton = document.getElementById("startQuizButton");
 let highScoreButton = document.getElementById("highScoreButton");
 let initialsSubmitButton = document.getElementById("initialsSubmitButton");
-  
-function getQuestionsScreen(){
-    mainScreen.style.display = "none"
-    questionsScreen.style.display = "flex"
+let goBackButton = document.getElementById("goBackButton");
+let clearHighScoreButton = document.getElementById("clearHighScoreButton");
+
+// options
+let option1 = document.getElementById("option1");
+let option2 = document.getElementById("option2");
+let option3 = document.getElementById("option3");
+let option4 = document.getElementById("option4");
+
+//Elements
+let question = document.getElementById("question");
+
+let questionsList = [
+  {
+    question:
+      "Which of the following is the correct syntax to print a page using JavaScript?",
+    options: [
+      "window.print();",
+      "browser.print();",
+      "navigator.print();",
+      "document.print();",
+    ],
+    answer: "window.print();",
+  },
+  {
+    question: "Which built-in method returns the length of the string?",
+    options: ["length()", "size()", "index()", "none of the above"],
+    answer: "length()",
+  },
+  {
+    question:
+      "Which built-in method returns the string representation of the number's value?",
+    options: ["toValue()", "toNumber()", "toString()", "none of the above"],
+    answer: "toString()",
+  },
+  {
+    question:
+      "Which function is used to serialize an object into a JSON string in Javascript?",
+    options: ["stringify()", "parse()", "convert()", "all of the above"],
+    answer: "stringify()",
+  },
+  {
+    question:
+      "Which of the following function of String object combines the text of two strings and returns a new string?",
+    options: ["add()", "merge()", "concat()", "append()"],
+    answer: "concat()",
+  },
+];
+
+function getQuestionsScreen() {
+  mainScreen.style.display = "none";
+  questionsScreen.style.display = "flex";
 }
-function gethighScoresScreen(){
-    quizFinishedScreen.style.display = "none"
-    topBar.style.display = "none"
-    highScoresScreen.style.display = "flex"
+var i = 1;
+function getAnswer(event) {
+  var userSelection = event.target.textContent;
+
+  if (questionsList[i - 1].options.includes(userSelection)) {
+    var isCorrect = false;
+    if (userSelection == questionsList[i - 1].answer) {
+      isCorrect = true;
+    }
+    if (i < questionsList.length) {
+      optionWrapper.innerHTML = "";
+      question.textContent = questionsList[i].question;
+      questionsList[i].options.forEach((optionText) => {
+        let option = document.createElement("div");
+        option.className = "option";
+        option.textContent = optionText;
+        optionWrapper.appendChild(option);
+      });
+
+      i++;
+      console.log(userSelection);
+    }
+
+
+
+  }
 }
 
+function gethighScoresScreen() {
+  mainScreen.style.display = "none";
+  quizFinishedScreen.style.display = "none";
+  questionsScreen.style.display = "none";
+  topBar.style.display = "none";
+  highScoresScreen.style.display = "flex";
+}
 
+function reload() {
+  location.reload();
+}
 
-
-startQuizButton.addEventListener('click', getQuestionsScreen)
-initialsSubmitButton.addEventListener('click', gethighScoresScreen)
+startQuizButton.addEventListener("click", getQuestionsScreen);
+initialsSubmitButton.addEventListener("click", gethighScoresScreen);
+highScoreButton.addEventListener("click", gethighScoresScreen);
+goBackButton.addEventListener("click", reload);
+optionWrapper.addEventListener("click", getAnswer);
