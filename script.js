14e66cc32952d5195de4df2e85294b2b98c02579
AleksandrBausher <bausheraleksandr@gmail.com>
2023-03-25 14:24:35 -0400

    designed high scores screen

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 //Screens
 let mainScreen = document.getElementById("mainScreen");
 let questionsScreen = document.getElementById("questionsScreen");
+let quizFinishedScreen = document.getElementById("quizFinishedScreen");
+let highScoresScreen = document.getElementById("highScoresScreen");
+let topBar = document.getElementById("topBar");
 
 //Buttons
 let startQuizButton = document.getElementById("startQuizButton");
 let highScoreButton = document.getElementById("highScoreButton");
-
-
-
-
-
-
+let initialsSubmitButton = document.getElementById("initialsSubmitButton");
+  
 function getQuestionsScreen(){
     mainScreen.style.display = "none"
     questionsScreen.style.display = "flex"
+}
+function gethighScoresScreen(){
+    quizFinishedScreen.style.display = "none"
+    topBar.style.display = "none"
+    highScoresScreen.style.display = "flex"
 }
 
 
 
 
 startQuizButton.addEventListener('click', getQuestionsScreen)
+initialsSubmitButton.addEventListener('click', gethighScoresScreen)
